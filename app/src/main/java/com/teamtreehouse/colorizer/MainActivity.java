926fd55ff52dec062ca0b94e2c73852f22f883d2
Joannalq <joanna851026@hotmail.com>
2017-05-15 18:48:05 +1000

    create menu option

---
+++ app/src/main/java/com/teamtreehouse/colorizer/MainActivity.java
@@ -0,0 +1,86 @@
+package com.teamtreehouse.colorizer;
+
+import android.graphics.Color;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    int[] imageResIds = {R.drawable.cuba1, R.drawable.cuba2, R.drawable.cuba3};
+    int imageIndex = 0;
+    boolean color = true;
+    boolean red = true;
+    boolean green = true;
+    boolean blue = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        loadImage();
+    }
+
+    private void loadImage() {
+        Glide.with(this).load(imageResIds[imageIndex]).into(imageView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /*
+        method1:adding items in code
+         */
+       /* MenuItem menuItem=menu.add("Change Image");
+        menuItem.setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS);
+        menuItem.setIcon(R.drawable.ic_insert_photo_black_24dp);
+        menuItem.getIcon().setColorFilter(Color.YELLOW, PorterDuff.Mode.SRC_ATOP);
+        */
+       //method2: using Menu resource
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        //change icon color
+        Drawable changeImageDrawable=menu.findItem(R.id.changeMenu).getIcon();
+        changeImageDrawable.setColorFilter(Color.YELLOW,PorterDuff.Mode.SRC_ATOP);
+
+        menu.findItem(R.id.blue).setChecked(blue);
+        menu.findItem(R.id.red).setChecked(red);
+        menu.findItem(R.id.green).setChecked(green);
+        return true;
+    }
+
+    private void updateSaturation() {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        if (color) {
+            red = green = blue = true;
+            colorMatrix.setSaturation(1);
+        } else {
+            colorMatrix.setSaturation(0);
+        }
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imageView.setColorFilter(colorFilter);
+    }
+
+    private void updateColors() {
+        ColorMatrix colorMatrix = new ColorMatrix();
+        float[] matrix = {
+                1, 0, 0, 0, 0,
+                0, 1, 0, 0, 0,
+                0, 0, 1, 0, 0,
+                0, 0, 0, 1, 0,
+        };
+        if (!red) matrix[0] = 0;
+        if (!green) matrix[6] = 0;
+        if (!blue) matrix[12] = 0;
+        colorMatrix.set(matrix);
+        ColorMatrixColorFilter colorFilter = new ColorMatrixColorFilter(colorMatrix);
+        imageView.setColorFilter(colorFilter);
+    }
+}
